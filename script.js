c89e22f8014e9542134a3c8fe81225bd3de9e15c
durbaon <nathanpaillet@gmail.com>
2024-02-13 15:41:16 +0100

    [REFACTOR] image resizing and convertToBadge function

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-//function to set the scale of the image
-function setScale(image) {
-    let scale = 0;
-
-    // Set the scale to the larger of the two dimensions
-    if (image.width > image.height) {
-        scale = image.width / image.height;
-    } else if (image.height > image.width) {
-        scale = image.height / image.width;
-    }
-    return scale
-}
-
 function applyOrangeFilter(context){
     const centerX = 256;
     const centerY = 256;
@@ -42,6 +29,40 @@ function applyOrangeFilter(context){
     }
 }
 
+function convertToBadge(image) {
+    const canvas = document.createElement('canvas');
+    const context = canvas.getContext('2d', { willReadFrequently: true });
+    canvas.width = 512;
+    canvas.height = 512;
+
+    // Check if the image is on the good size
+    if (image.width != 512 || image.height != 512) {
+        alert("The image is not on the good size and will be resize ")
+    }
+
+    // Calculate the scale of the image
+    let scale = Math.max(image.width / image.height, image.height / image.width);
+
+    // Clip the canvas to a circle
+    context.save();
+    context.arc(256, 256, 256, 0, Math.PI * 2);
+    context.clip();
+
+    // Draw the image on the canvas with the correct scale
+    if (image.width > image.height) {
+        context.drawImage(image, 128 * (scale - 1), 0, image.width, image.height, 0, 0, 512 * scale, 512);
+    } else if (image.height > image.width) {
+        context.drawImage(image, 0, 128 * (scale - 1), image.width, image.height, 0, 0, 512, 512 * scale);
+    } else {
+        context.drawImage(image, 0, 0, 512, 512);
+    }
+
+    // Apply the orange filter
+    applyOrangeFilter(context);
+
+    return context.getImageData(0, 0, 512, 512);
+}
+
 // Get the file input element
 const input = document.getElementById('fileInput');
 
@@ -56,40 +77,12 @@ input.addEventListener('change', function() {
 
     const image = new Image();
     image.onload = function() {
-        const canvas = document.createElement('canvas');
-        const context = canvas.getContext('2d', { willReadFrequently: true });
-        canvas.width = 512;
-        canvas.height = 512;
-
-        // Check if the image is on the good size
-        if (image.width != 512 || image.height != 512) {
-            alert("The image is not on the good size and will be resize ")
-        }
-
-        let scale = setScale(image)
-
-        // Clip the canvas to a circle
-        context.save();
-        context.arc(256, 256, 256, 0, Math.PI * 2);
-        context.clip();
-
-        // Draw the image on the canvas with the correct scale
-        if (image.width > image.height) {
-            context.drawImage(image, 128 * (scale - 1), 0, image.width, image.height, 0, 0, 512 * scale, 512);
-        } else if (image.height > image.width) {
-            context.drawImage(image, 0, 128 * (scale - 1), image.width, image.height, 0, 0, 512, 512 * scale);
-        } else {
-            context.drawImage(image, 0, 0, 512, 512);
-        }
-
-        // Apply the orange filter
-        applyOrangeFilter(context);
-
-        const imageData = context.getImageData(0, 0, 512, 512);
+        const imageData = convertToBadge(image);
         const transformedCanvas = document.getElementById('transformedCanvas');
         const ctx = transformedCanvas.getContext('2d');
         ctx.putImageData(imageData, 0, 0);
     };
+
     // Load the image on the canvas
     image.src = URL.createObjectURL(file);
 });
